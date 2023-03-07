5015eff2905c56a6b2b527c62aaf222588c5f029
Aaron Criner <aaron@Aarons-MacBook-Pro.local>
2023-03-07 11:55:26 -0800

    added player turn functionality

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import "./App.css";
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState("❌");
 
   const clickHandle = (index) => {
     let updatedBoard = [...squares];
-    console.log("clicked:", index);
-    updatedBoard[index] = "❌";
-    setSquares(updatedBoard);
+    if (playerTurn === "❌") {
+      updatedBoard[index] = "❌";
+      setSquares(updatedBoard);
+      setPlayerTurn("⭕️");
+    } else if (playerTurn === "⭕️") {
+      updatedBoard[index] = "⭕️";
+      setSquares(updatedBoard);
+      setPlayerTurn("❌");
+    }
   };
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <h2> Current Player: {playerTurn}</h2>
       <div className="gameBoard">
         {squares.map((value, index) => {
           return (
--- src/components/Square.js
+++ src/components/Square.js
@@ -4,6 +4,7 @@ const Square = ({ index, clickHandle, value }) => {
   const handleClick = () => {
     clickHandle(index);
   };
+
   return (
     <div className={"square"} id={`square${index}`} onClick={handleClick}>
       {value}
